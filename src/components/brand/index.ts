0a04595ee0d6a62ee2f364da4a00ed6bc25274ba
Samuel François <samuel.francois.pro@gmail.com>
2026-05-06 21:40:53 +0200

    feat(brand): barrel export src/components/brand

---
+++ src/components/brand/index.ts
@@ -0,0 +1,18 @@
+// src/components/brand/index.ts
+export { Eyebrow } from "./Eyebrow";
+export { SectionHeader } from "./SectionHeader";
+export { ProBadge } from "./ProBadge";
+export { PriceTag } from "./PriceTag";
+export { DataRow } from "./DataRow";
+export { KPICard } from "./KPICard";
+export { EmptyState } from "./EmptyState";
+export { BrandLogo } from "./BrandLogo";
+
+export type { EyebrowProps } from "./Eyebrow";
+export type { SectionHeaderProps } from "./SectionHeader";
+export type { ProBadgeProps } from "./ProBadge";
+export type { PriceTagProps } from "./PriceTag";
+export type { DataRowProps } from "./DataRow";
+export type { KPICardProps } from "./KPICard";
+export type { EmptyStateProps } from "./EmptyState";
+export type { BrandLogoProps } from "./BrandLogo";
